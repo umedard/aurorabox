57801c6e9ac05c9b108b0785603c0b6c9feac0a5
Medard <umedard@gmail.com>
2020-08-31 19:18:12 +0300

    close bucket by clicking outside of it

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -1,5 +1,19 @@
 "use strict";
 
-document.querySelector("#busket").addEventListener("click", function () {
-  document.querySelector("#drop").classList.toggle("open");
-});
+function toggleBasket() {
+  document.querySelector("#busket").addEventListener("click", function () {
+    document.querySelector("#drop").classList.toggle("open");
+  });
+  document.addEventListener("click", function (_ref) {
+    var target = _ref.target;
+
+    if (!target.closest(".header-middle__busket")) {
+      document.querySelector("#drop").classList.remove("open");
+    }
+  });
+  document.querySelector("#drop").addEventListener("click", function (e) {
+    e.stopPropagation();
+  });
+}
+
+window.addEventListener("load", toggleBasket);
--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,17 @@
-document.querySelector("#busket").addEventListener("click", () => {
-  document.querySelector("#drop").classList.toggle("open");
-});
+function toggleBasket() {
+  document.querySelector("#busket").addEventListener("click", () => {
+    document.querySelector("#drop").classList.toggle("open");
+  });
+
+  document.addEventListener("click", ({ target }) => {
+    if (!target.closest(".header-middle__busket")) {
+      document.querySelector("#drop").classList.remove("open");
+    }
+  });
+
+  document.querySelector("#drop").addEventListener("click", (e) => {
+    e.stopPropagation();
+  });
+}
+
+window.addEventListener("load", toggleBasket);
